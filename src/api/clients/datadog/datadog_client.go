aad489067476ce6a2c4e6383cf1f727ec1af29a1
ekohmann <emiliano.kohmann@mercadolibre.com>
2019-03-13 04:07:37 -0300

    Adding dependencies

--- src/api/clients/datadog/datadog_client.go
+++ src/api/clients/datadog/datadog_client.go
@@ -12,22 +12,29 @@ const (
     applicationNameTag     = "application_name:%s"
 )
 
+var (
+    client *statsd.Client
+)
+
+func init() {
+    client, err := statsd.New(config.DatadogMetricAddress)
+    if err != nil {
+        config.Logger.Println(errSendingMetricToDD, err)
+        panic(err)
+    }
+    // prefix every metric with the app name
+    client.Namespace = fmt.Sprintf(applicationNamePattern, config.ApplicationName)
+    // send every metric with application name tag
+    client.Tags = append(client.Tags, fmt.Sprintf(applicationNameTag, config.ApplicationName))
+}
+
 func IncrementSimpleApplicationMetric(metricName string, tags ...string) {
     IncrementApplicationMetric(metricName, 1, tags...)
 }
 
 func IncrementApplicationMetric(metricName string, value float64, tags ...string) {
     go func() {
-        c, err := statsd.New(config.DatadogMetricAddress)
-        if err != nil {
-            config.Logger.Println(errSendingMetricToDD, err)
-            return
-        }
-        // prefix every metric with the app name
-        c.Namespace = fmt.Sprintf(applicationNamePattern, config.ApplicationName)
-        // send every metric with application name tag
-        c.Tags = append(c.Tags, fmt.Sprintf(applicationNameTag, config.ApplicationName))
-        if err = c.Gauge(metricName, value, tags, 1); err != nil {
+        if err := client.Gauge(metricName, value, tags, 1); err != nil {
             config.Logger.Println(errSendingMetricToDD, err)
             return
         }
